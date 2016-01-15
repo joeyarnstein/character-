35c2ba6418e4a16d3741871b70df0a96bded5282
Joey Arnstein and Matt Rosanio <joeyarstein@gmail.com>
2016-01-14 16:02:42 -0800

    Add more options for characters

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
 $(function(){
-  $('#blanks form').submit(function() {
+  $('#blanks form').submit(function(event) {
     var sex = $("select#gender").val();
     var morality = parseInt($("select#goodbad").val());
-    debugger;
     if (sex === "male") {
       if (morality > 6) {
-      
+        $(".soul").text("Ned Stark");
+      } else if (morality > 3) {
+        $(".soul").text("Tyrion Lannister");
+      } else {
+        $(".soul").text("Roose Bolton");
+      }
+    } else {
+      if (morality > 6) {
+        $(".soul").text("Danerys Targarian");
+      } else if (morality > 3) {
+        $(".soul").text("Sansa Stark");
+      } else {
+        $(".soul").text("Cercei Lannister");
       }
     }
+    event.preventDefault();
   })
 });
